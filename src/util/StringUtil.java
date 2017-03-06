29450a4a56b754f90fd9a39c2e8bbaf4cdbfcad0
Hotlink Qiu <hotlinkqiu@gmail.com>
2017-03-06 14:03:19 +0800

    some string utility

--- src/util/StringUtil.java
+++ src/util/StringUtil.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class StringUtil {
 	public static String deBracket(String str) {
 		String deb = "";
@@ -11,5 +14,29 @@ public class StringUtil {
 			deb = str;
 		}
 		return deb;
+	}
+	
+	public static int getProbelmIndex(String str) {
+		if(str.startsWith("[P_") && str.endsWith("]")) {
+			return Integer.parseInt(str.substring(3, str.length()-1))-1;
+		} else {
+			return -1;
+		}
+	}
+	
+	public static String encodeURIComponent(String str) {
+		String result = null;
+		try {
+			result = URLEncoder.encode(str, "UTF-8")
+							   .replaceAll("\\+", "%20")
+							   .replaceAll("\\%21", "!")
+							   .replaceAll("\\%27", "'")
+							   .replaceAll("\\%28", "(")
+							   .replaceAll("\\%29", ")")
+							   .replaceAll("\\%7E", "~");
+		} catch(UnsupportedEncodingException e) {
+			result = str;
+		}
+		return result;
 	}
 }
