de2d64f1f9c70a9f9160ee2be85f16ab6030e335
Hotlink Qiu <hotlinkqiu@gmail.com>
2017-03-13 13:20:37 +0800

    add sentence tree url

--- src/buffer/ProblemProcBuffer.java
+++ src/buffer/ProblemProcBuffer.java
@@ -3,6 +3,8 @@ package buffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import util.StringUtil;
+
 public class ProblemProcBuffer {
 	List<String> problemProc;
 
@@ -11,11 +13,24 @@ public class ProblemProcBuffer {
 	}
 	
 	public void addProblemProc(int pno, String proc) {
+		if(isTree(proc)) proc = parseTree(proc);
 		if(pno == problemProc.size()) {
 			problemProc.add(proc);
 		} else {
 			problemProc.set(pno, problemProc.get(pno)+"<br/>"+proc);
 		}
+	}
+	
+	private boolean isTree(String solver) {
+		if(solver.contains("句法树]")) return true;
+		return false;
+	}
+	
+	private String parseTree(String solver) {
+		String[] elements = solver.split("\t");
+		String uri = StringUtil.encodeURIComponent(elements[2]);
+		String treeLink = "<a href='./tree.html?sentence=" + uri + "' target='_blank'>" + elements[2] + "</a>";
+		return elements[0] + "\t" + elements[1] + "\t" + treeLink;
 	}
 	
 	public String getProblemProc(int pno) {
